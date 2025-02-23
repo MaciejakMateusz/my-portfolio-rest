054c69ba72c083c449f5a34500fc12f9223d38de
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-02-23 18:59:49 +0100

    Updated ToleranceMeasureController

--- src/main/java/pl/maciejak/my_portfolio_rest/controller/ToleranceMeasureController.java
+++ src/main/java/pl/maciejak/my_portfolio_rest/controller/ToleranceMeasureController.java
@@ -1,21 +1,25 @@
 package pl.maciejak.my_portfolio_rest.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import pl.maciejak.my_portfolio_rest.dto.MeasurementsDTO;
+import pl.maciejak.my_portfolio_rest.service.interfaces.ToleranceMeasureService;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/tolerance-measure")
 public class ToleranceMeasureController {
 
+    private final ToleranceMeasureService toleranceMeasureService;
+
     @PostMapping("/calculate")
-    public ResponseEntity<?> calculate(@RequestBody List<Integer> measurements) {
-        return ResponseEntity.ok(measurements);
+    public ResponseEntity<?> calculate(@RequestBody MeasurementsDTO measurementsDTO) {
+        return toleranceMeasureService.calculate(measurementsDTO);
     }
 
     @RequestMapping(method = RequestMethod.OPTIONS)
