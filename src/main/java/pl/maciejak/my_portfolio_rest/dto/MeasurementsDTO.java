6b763178ed65eeca31d38cf3f758b51426f8a3be
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-02 15:33:10 +0100

    Updated MeasurementsDTO, converted to BigDecimal

--- src/main/java/pl/maciejak/my_portfolio_rest/dto/MeasurementsDTO.java
+++ src/main/java/pl/maciejak/my_portfolio_rest/dto/MeasurementsDTO.java
@@ -1,9 +1,10 @@
 package pl.maciejak.my_portfolio_rest.dto;
 
+import java.math.BigDecimal;
 import java.util.List;
 
-public record MeasurementsDTO(Double productLength,
-                              Double negTolerance,
-                              Double posTolerance,
-                              List<Double> measurements) {
+public record MeasurementsDTO(BigDecimal productLength,
+                              BigDecimal negTolerance,
+                              BigDecimal posTolerance,
+                              List<BigDecimal> measurements) {
 }
