936ef53da3461616204509b44106591d022f31ca
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-02 18:38:08 +0100

    Added font that supports special characters, updated PdfGenerator

--- src/main/java/pl/maciejak/my_portfolio_rest/util/PdfGenerator.java
+++ src/main/java/pl/maciejak/my_portfolio_rest/util/PdfGenerator.java
@@ -2,12 +2,15 @@ package pl.maciejak.my_portfolio_rest.util;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 @Slf4j
 @Component
@@ -20,9 +23,13 @@ public class PdfGenerator {
             Document document = new Document();
             PdfWriter.getInstance(document, baos);
             document.open();
-            document.add(new Paragraph(reportContent));
+
+            BaseFont baseFont = BaseFont.createFont("src/main/resources/fonts/DejaVuSans.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            Font font = new Font(baseFont, 12, Font.NORMAL);
+            document.add(new Paragraph(reportContent, font));
+
             document.close();
-        } catch (DocumentException e) {
+        } catch (DocumentException | IOException e) {
             log.error("Error generating PDF: {}", e.getMessage(), e);
             throw new RuntimeException("PDF generation failed", e);
         }
