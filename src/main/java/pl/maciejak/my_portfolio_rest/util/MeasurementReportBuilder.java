4858e3bb9c8f444edc66dea8a798ef280852c3e9
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-02 18:39:51 +0100

    Updated MeasurementReportBuilder, used MsgProvider to get localized messages based on request locale header

--- src/main/java/pl/maciejak/my_portfolio_rest/util/MeasurementReportBuilder.java
+++ src/main/java/pl/maciejak/my_portfolio_rest/util/MeasurementReportBuilder.java
@@ -1,65 +1,90 @@
 package pl.maciejak.my_portfolio_rest.util;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import pl.maciejak.my_portfolio_rest.dto.MeasurementAnalysis;
 import pl.maciejak.my_portfolio_rest.dto.MeasurementsDTO;
 
 @Component
+@RequiredArgsConstructor
 public class MeasurementReportBuilder {
+
+    private final MsgProvider msgProvider;
 
     public String buildReport(MeasurementsDTO measurementsDTO, MeasurementAnalysis analysis) {
         StringBuilder result = new StringBuilder();
         String formattedDate = analysis.reportDate();
 
-        result.append("Measurement session date: ").append(formattedDate).append("\n\n");
+        result.append(msgProvider.getLocalizedMsg("tolMeasure.sessionDate", null))
+                .append(formattedDate)
+                .append(" \n\n");
 
-        result.append("All measurements (mm): ").append(measurementsDTO.measurements()).append("\n\n");
+        result.append(msgProvider.getLocalizedMsg("tolMeasure.productData", null))
+                .append("\n");
+        result.append(msgProvider.getLocalizedMsg("tolMeasure.productLength", null))
+                .append(analysis.productLength())
+                .append(" mm\n");
+        result.append(msgProvider.getLocalizedMsg("tolMeasure.positiveTolerance", null))
+                .append(analysis.posTolerance())
+                .append(" mm\n");
+        result.append(msgProvider.getLocalizedMsg("tolMeasure.negativeTolerance", null))
+                .append(analysis.negTolerance())
+                .append(" mm\n\n");
 
-        result.append("Amount measured: ")
+        result.append(msgProvider.getLocalizedMsg("tolMeasure.measurementsData", null))
+                .append("\n");
+        result.append(msgProvider.getLocalizedMsg("tolMeasure.allMeasurements", null))
+                .append(measurementsDTO.measurements())
+                .append("\n\n");
+
+        result.append(msgProvider.getLocalizedMsg("tolMeasure.amountMeasured", null))
                 .append(analysis.totalCount())
-                .append(" piece(s)\n");
+                .append(msgProvider.getLocalizedMsg("tolMeasure.pieces", null))
+                .append("\n");
 
-        result.append("Average of all measurements: ")
+        result.append(msgProvider.getLocalizedMsg("tolMeasure.average", null))
                 .append(String.format("%.2f", analysis.average()))
-                .append("mm\n\n");
+                .append(" mm\n\n");
 
-        result.append("Outside tolerance: ")
+        result.append(msgProvider.getLocalizedMsg("tolMeasure.outsideTolerance", null))
                 .append(analysis.outsideTolerance())
-                .append(" piece(s)\n");
+                .append(msgProvider.getLocalizedMsg("tolMeasure.pieces", null))
+                .append("\n");
 
-        result.append("Inside tolerance: ")
+        result.append(msgProvider.getLocalizedMsg("tolMeasure.insideTolerance", null))
                 .append(analysis.insideTolerance())
-                .append(" piece(s)\n");
+                .append(msgProvider.getLocalizedMsg("tolMeasure.pieces", null))
+                .append("\n");
 
-        result.append("Greater than ")
+        result.append(msgProvider.getLocalizedMsg("tolMeasure.greaterThan", null))
                 .append(analysis.upperBound())
-                .append("mm: ")
+                .append(" mm: ")
                 .append(analysis.graterThanUpperBound())
-                .append(" piece(s)\n");
+                .append(msgProvider.getLocalizedMsg("tolMeasure.pieces", null))
+                .append("\n");
 
-        result.append("Smaller than ")
+        result.append(msgProvider.getLocalizedMsg("tolMeasure.smallerThan", null))
                 .append(analysis.lowerBound())
-                .append("mm: ")
+                .append(" mm: ")
                 .append(analysis.smallerThanLowerBound())
-                .append(" piece(s)\n\n");
+                .append(msgProvider.getLocalizedMsg("tolMeasure.pieces", null))
+                .append("\n\n");
 
-        result.append("Greatest measurement: ")
+        result.append(msgProvider.getLocalizedMsg("tolMeasure.greatestMeasurement", null))
                 .append(analysis.maxMeasurement())
-                .append("mm\n");
+                .append(" mm\n");
 
-        result.append("Smallest measurement: ")
+        result.append(msgProvider.getLocalizedMsg("tolMeasure.smallestMeasurement", null))
                 .append(analysis.minMeasurement())
-                .append("mm\n");
+                .append(" mm\n");
 
-        result.append("The difference between the smallest and the biggest measurement: ")
+        result.append(msgProvider.getLocalizedMsg("tolMeasure.difference", null))
                 .append(analysis.difference())
-                .append("mm\n\n");
+                .append(" mm\n\n");
 
-        result.append("Measurements sorted ascending: ")
+        result.append(msgProvider.getLocalizedMsg("tolMeasure.sortedMeasurements", null))
                 .append(analysis.sortedMeasurements());
 
         return result.toString();
     }
-
-
 }
