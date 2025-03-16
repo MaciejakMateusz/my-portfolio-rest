98c7bfe76648de89309e3c411481f82e092c2c0b
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-16 12:22:45 +0100

    Added validation logic to ToleranceMeasureController

--- src/main/java/pl/maciejak/my_portfolio_rest/controller/ToleranceMeasureController.java
+++ src/main/java/pl/maciejak/my_portfolio_rest/controller/ToleranceMeasureController.java
@@ -1,10 +1,12 @@
 package pl.maciejak.my_portfolio_rest.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import pl.maciejak.my_portfolio_rest.dto.MeasurementsDTO;
 import pl.maciejak.my_portfolio_rest.service.interfaces.ToleranceMeasureService;
@@ -16,9 +18,13 @@ import pl.maciejak.my_portfolio_rest.service.interfaces.ToleranceMeasureService;
 public class ToleranceMeasureController {
 
     private final ToleranceMeasureService toleranceMeasureService;
+    private final ResponseHelper responseHelper;
 
     @PostMapping("/calculate")
-    public ResponseEntity<?> calculate(@RequestBody MeasurementsDTO measurementsDTO) {
+    public ResponseEntity<?> calculate(@RequestBody @Valid MeasurementsDTO measurementsDTO, BindingResult br) {
+        if (br.hasErrors()) {
+            return ResponseEntity.badRequest().body(responseHelper.getFieldErrors(br));
+        }
         return toleranceMeasureService.calculate(measurementsDTO);
     }
 
--- src/main/java/pl/maciejak/my_portfolio_rest/dto/MeasurementsDTO.java
+++ src/main/java/pl/maciejak/my_portfolio_rest/dto/MeasurementsDTO.java
@@ -1,10 +1,12 @@
 package pl.maciejak.my_portfolio_rest.dto;
 
+import jakarta.validation.constraints.*;
+
 import java.math.BigDecimal;
 import java.util.List;
 
-public record MeasurementsDTO(BigDecimal productLength,
-                              BigDecimal negTolerance,
-                              BigDecimal posTolerance,
-                              List<BigDecimal> measurements) {
+public record MeasurementsDTO(@NotNull @Min(1) BigDecimal productLength,
+                              @NotNull @NegativeOrZero BigDecimal negTolerance,
+                              @NotNull @PositiveOrZero BigDecimal posTolerance,
+                              @NotEmpty @Size(min = 2) List<BigDecimal> measurements) {
 }
