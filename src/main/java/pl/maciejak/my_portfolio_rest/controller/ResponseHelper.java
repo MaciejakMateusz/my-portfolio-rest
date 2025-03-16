cfb5409171bd9f1cde7059f64c0fbe59e1ce955e
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-03-16 12:21:59 +0100

    Created basic ResponseHelper

---
+++ src/main/java/pl/maciejak/my_portfolio_rest/controller/ResponseHelper.java
@@ -0,0 +1,36 @@
+package pl.maciejak.my_portfolio_rest.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This component provides helper methods to build standardized ResponseEntity objects
+ * for various scenarios (e.g., saving entities, retrieving data, handling errors, and redirects).
+ * It encapsulates common patterns of exception handling and result packaging to streamline
+ * controller code.
+ */
+@Component
+@Slf4j
+public class ResponseHelper {
+
+    /**
+     * Extracts field errors from the provided BindingResult.
+     * <p>
+     * This method should be called after checking {@code br.hasErrors()}.
+     *
+     * @param br the BindingResult containing field errors
+     * @return a map where the keys are field names and the values are the corresponding error messages
+     */
+    public Map<String, String> getFieldErrors(BindingResult br) {
+        Map<String, String> fieldErrors = new HashMap<>();
+        for (FieldError error : br.getFieldErrors()) {
+            fieldErrors.put(error.getField(), error.getDefaultMessage());
+        }
+        return fieldErrors;
+    }
+}
